facc5a5ea6ca42d2288ce0cb80cb448f66bd9d7a
Olexandr Lazarenko <99609396+AleXLaeR@users.noreply.github.com>
2023-05-15 12:30:05 +0300

    Update useClickOutside.ts

--- hooks/useClickOutside.ts
+++ hooks/useClickOutside.ts
@@ -1,35 +1,41 @@
 import { RefObject, useCallback, useEffect, useRef } from 'react';
 
+type EventCallback = (e?: Event) => void;
+
 export default function useClickOutside<El extends HTMLElement>(
   elemRef: RefObject<El>,
-  handler: (e: Event) => void,
+  callback: EventCallback,
   parentRef?: RefObject<El>,
 ): void {
-  const handlerRef = useRef(handler);
+  const callbackRef = useRef(callback);
 
-  const listener = useCallback(
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  const handler = useCallback(
     (event: Event) => {
       const nodeTarget = event.target as Node;
 
-      if (parentRef && !parentRef.current?.contains(nodeTarget)) {
+      if (parentRef?.current && !parentRef.current.contains(nodeTarget)) {
         return;
       }
       if (!elemRef.current || elemRef.current.contains(nodeTarget)) {
         return;
       }
 
-      handlerRef.current(event);
+      callbackRef.current(event);
     },
-    [parentRef, elemRef],
+    [elemRef, parentRef],
   );
 
   useEffect(() => {
-    window.addEventListener('mouseup', listener);
-    window.addEventListener('touchstart', listener);
+    window.addEventListener('mouseup', handler);
+    window.addEventListener('touchstart', handler);
 
     return () => {
-      window.removeEventListener('mouseup', listener);
-      window.removeEventListener('touchstart', listener);
+      window.removeEventListener('mouseup', handler);
+      window.removeEventListener('touchstart', handler);
     };
   });
 }
