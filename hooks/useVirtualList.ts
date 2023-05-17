cde31f374a2489d338848f379be7fb515b0045af
Olexandr Lazarenko <99609396+AleXLaeR@users.noreply.github.com>
2023-05-17 11:08:46 +0300

    Update useVirtualList.ts

--- hooks/useVirtualList.ts
+++ hooks/useVirtualList.ts
@@ -28,22 +28,48 @@ export default function useVirtualList<T>(
     const containerHeight = containerRef.current.clientHeight;
     const visibleCount = Math.ceil(containerHeight / itemHeight);
 
-    const start = Math.floor(scrollTop / itemHeight);
-    const end = start + visibleCount + overScan * 2;
+    const startIdx = Math.max(0, Math.floor(scrollTop / itemHeight));
+    const endIdx = Math.min(initialList.length, startIdx + visibleCount + overScan * 2);
 
-    setVisibleItems(initialList.slice(start, end));
+    setVisibleItems(initialList.slice(startIdx, endIdx));
   }, [initialList, itemHeight, overScan, scrollTop]);
+
+  const handleScroll = useCallback(() => {
+    if (!containerRef.current) return;
+
+    requestAnimationFrame(() => {
+      setScrollTop(containerRef.current!.scrollTop);
+    });
+  }, []);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    container.addEventListener('scroll', handleScroll);
+
+    return () => {
+      container.removeEventListener('scroll', handleScroll);
+    };
+  }, [handleScroll]);
 
   const scrollTo = useCallback(
     (index: number) => {
       if (!containerRef.current) return;
 
-      const top = index * itemHeight;
+      const maxScrollTop = Math.max(
+        0,
+        (initialList.length - 1) * itemHeight - containerRef.current.clientHeight,
+      );
+      const targetScrollTop = Math.min(index * itemHeight, maxScrollTop);
 
-      containerRef.current.scrollTop = top;
-      setScrollTop(top);
+      containerRef.current.scrollTo({
+        top: targetScrollTop,
+        behavior: 'smooth',
+      });
+      setScrollTop(targetScrollTop);
     },
-    [itemHeight],
+    [itemHeight, initialList],
   );
 
   return [containerRef, visibleItems, scrollTo] as const;
